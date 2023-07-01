5f32a7cc496f659e12e542afecf176a58b1b556f
vitali <vitali.slepnyov@gmail.com>
2023-07-01 23:50:35 +0300

    feat: добавить компонент app

---
+++ src/components/аpp/app.jsx
@@ -0,0 +1,21 @@
+
+import AppHeader from "../app-header/app-header";
+import BurgerIngredients from "../burger-ingredients/burger-ingredients";
+import BurgerConstructor from "../burger-constructor/burger-constructor";
+// import styleApp from "./app.module.css"
+
+function App() {
+
+  return (
+    <div>
+      <AppHeader />
+      <main>
+        <BurgerIngredients />
+        <BurgerConstructor />
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
