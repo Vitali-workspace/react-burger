d07e6cca5e32b43b7d1f892db2a980086c865be8
vitali <vitali.slepnyov@gmail.com>
2023-07-02 23:39:18 +0300

    feat: добавить фукцию запроса ингредиентов

--- src/components/аpp/app.jsx
+++ src/components/аpp/app.jsx
@@ -1,16 +1,33 @@
 
+import { useState, useEffect } from 'react';
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
-// import styleApp from "./app.module.css"
+import styleApp from "./app.module.css"
+const API_URL = "https://norma.nomoreparties.space/api/ingredients";
 
 function App() {
+
+  const [listIngredients, setListIngredients] = useState([]);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(data => setListIngredients(data.data))
+      .catch((res) => {
+        if (!res.ok) {
+          return Promise.reject(`произошла ошибка: ${res.status}`);
+        }
+        return res.json();
+      });
+  }, []);
+
 
   return (
     <div>
       <AppHeader />
-      <main>
-        <BurgerIngredients />
+      <main className={styleApp.content}>
+        <BurgerIngredients data={listIngredients} />
         <BurgerConstructor />
       </main>
     </div>
