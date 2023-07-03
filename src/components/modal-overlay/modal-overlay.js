57d129724298779ad231ce3acbea02c497763754
vitali <vitali.slepnyov@gmail.com>
2023-07-03 23:54:01 +0300

    feat: добавить компонент  ModalOverlay и стили

---
+++ src/components/modal-overlay/modal-overlay.js
@@ -0,0 +1,10 @@
+import styleOverlay from "./modal-overlay.module.css"
+
+function ModalOverlay() {
+
+  return (
+    <section className={styleOverlay.overlay}></section>
+  );
+}
+
+export default ModalOverlay;
