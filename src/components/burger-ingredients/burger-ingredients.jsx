61bf01454ec479906e90b5c9ee7302a8e8e75850
vitali <vitali.slepnyov@gmail.com>
2023-07-03 17:22:04 +0300

    feat: добавить Tab меню

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 import Ingredient from "../ingredient/ingredient";
 import styleIngredients from "./burger-ingredients.module.css";
@@ -8,12 +9,24 @@ function BurgerIngredients({ data }) {
   const listBuns = data.filter((item) => item.type === "bun");
   const listSauces = data.filter((item) => item.type === "sauce");
   const listMains = data.filter((item) => item.type === "main");
+  const [current, setCurrent] = useState('Булки');
 
   return (
-    <section className={styleIngredients.container}>
-      <div className={styleIngredients.block + " mr-10"}>
+    <section className={styleIngredients.container + " mr-10"}>
+      <div className={styleIngredients.block}>
         <h1 className="text text_type_main-large mt-10 mb-5">Соберите бургер</h1>
-        <Tab>{'Булки'}</Tab>
+
+        <div className={styleIngredients.tab}>
+          <Tab value="Булки" active={current === 'Булки'} onClick={setCurrent}>
+            Булки
+          </Tab>
+          <Tab value="Соусы" active={current === 'Соусы'} onClick={setCurrent}>
+            Соусы
+          </Tab>
+          <Tab value="Начинки" active={current === 'Начинки'} onClick={setCurrent}>
+            Начинки
+          </Tab>
+        </div>
 
         <div className={styleIngredients.content + " mt-10"}>
           <div>
