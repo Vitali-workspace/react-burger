13c668c0d1fe40ea103a754e0594061e0a747d47
vitali <vitali.slepnyov@gmail.com>
2023-07-05 00:18:53 +0300

    feat: добавить обработчики событий для модального окна

--- src/components/modal-overlay/modal-overlay.js
+++ src/components/modal-overlay/modal-overlay.js
@@ -1,10 +1,16 @@
+import PropTypes from 'prop-types';
 import styleOverlay from "./modal-overlay.module.css"
 
-function ModalOverlay() {
+function ModalOverlay(props) {
 
   return (
-    <section className={styleOverlay.overlay}></section>
+    <section
+      className={styleOverlay.overlay}
+      onClick={props.overlayCloseClick}>
+    </section>
   );
 }
+
+ModalOverlay.propTypes = { overlayCloseClick: PropTypes.func }
 
 export default ModalOverlay;
--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,27 +1,47 @@
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
-import styleModal from "./modal.module.css"
+import PropTypes from 'prop-types';
+import ModalOverlay from '../modal-overlay/modal-overlay';
+import styleModal from "./modal.module.css";
+
 
 
 function Modal(props) {
 
-  function mods() {
+  useEffect(() => {
+    const handleKeyDown = function (e) {
+      if (e.key === 'Escape') {
+        props.handleCloseClick();
+      }
+    };
+    document.addEventListener('keydown', handleKeyDown);
 
-    props.handleCloseClick();
-  }
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
 
   return createPortal(
-    <section className={styleModal.container}>
-      <div className={styleModal.popup}>
-        <button className={styleModal.buttonClose} onClick={props.handleCloseClick}>
-          <CloseIcon type="primary" />
-        </button>
-        {props.children}
-      </div>
-    </section>,
+    <>
+      <ModalOverlay overlayCloseClick={props.handleCloseClick} />
+      <section className={styleModal.container}>
+        <div className={styleModal.popup}>
+          <button className={styleModal.buttonClose} onClick={props.handleCloseClick}>
+            <CloseIcon type="primary" />
+          </button>
+          {props.children}
+        </div>
+      </section>
+    </>,
 
     props.pointModal
   );
+}
+
+Modal.propTypes = {
+  handleCloseClick: PropTypes.func,
+  pointModal: PropTypes.node,
 }
 
 export default Modal;
