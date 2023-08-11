f2e4ae2acdf8a879beaf0e40f4231cd6a0c8fe64
vitali <vitali.slepnyov@gmail.com>
2023-08-11 23:53:21 +0300

    feat: добавить функцию getOrderNumber и подключить контекст

--- src/components/аpp/app.jsx
+++ src/components/аpp/app.jsx
@@ -5,13 +5,16 @@ import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
 import styleApp from "./app.module.css"
 import API_URL from '../../utils/constants';
+import { BurgerConstructorContext } from '../../services/context/burger-constructor-context';
 
 const modalRoot = document.getElementById('root');
+const API_URL_ORDER = "https://norma.nomoreparties.space/api/orders"; //! test
 
 
 function App() {
 
   const [listIngredients, setListIngredients] = useState([]);
+  const [statusOrder, setStatusOrder] = useState({});
 
   useEffect(() => {
     fetch(API_URL)
@@ -23,19 +26,37 @@ function App() {
       })
       .then(data => setListIngredients(data.data))
       .catch(console.error);
-
   }, []);
 
+
+  function getOrderNumber(listId) {
+    console.log(listId)
+    return fetch(API_URL_ORDER, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ "ingredients": listId }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        return Promise.reject(`произошла ошибка: ${response.status}`);
+      })
+      .then(data => setStatusOrder(data))
+      .catch(console.error);
+  }
 
   return (
     <div>
       <AppHeader />
       <main className={styleApp.content}>
-        <BurgerIngredients data={listIngredients} itemDom={modalRoot} />
-        {
-          listIngredients.length > 0 &&
-          <BurgerConstructor data={listIngredients} itemDom={modalRoot} />
-        }
+        <BurgerConstructorContext.Provider value={{ listIngredients, getOrderNumber, statusOrder }}>
+          {/* <BurgerIngredients data={listIngredients} itemDom={modalRoot} /> */}
+          {
+            listIngredients.length > 0 &&
+            <BurgerConstructor itemDom={modalRoot} />
+          }
+        </BurgerConstructorContext.Provider>
       </main>
     </div>
   );
