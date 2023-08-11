c2cb83e117aed479ad02d2713db3e963f3de1c4f
vitali <vitali.slepnyov@gmail.com>
2023-08-11 23:54:47 +0300

    feat: добавить  отображение номера заказа

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,19 +1,32 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { Button, ConstructorElement, CurrencyIcon, DragIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import PropTypes from 'prop-types';
 import Modal from "../modal/modal";
 import OrderDetails from "../order-details/order-details";
 import styleConstructor from "./burger-constructor.module.css";
+import { BurgerConstructorContext } from "../../services/context/burger-constructor-context";
 
 
-function BurgerConstructor({ data, itemDom }) {
+function BurgerConstructor({ itemDom }) {
 
   const [openPopup, setOpenPopup] = useState(false);
-  const saucesAndMains = data.filter((item) => item.type !== "bun");
+  const constructorContext = useContext(BurgerConstructorContext);
+
+  const list = constructorContext.listIngredients;
+  const saucesAndMains = list.filter((item) => item.type !== "bun");
+
+
+
+  const totalPrice = saucesAndMains.reduce((acc, curr) => acc + curr.price, 0);
 
   function handleOrderClick() {
     setOpenPopup(!openPopup);
   }
+
+  function handleOrderNumber() {
+    constructorContext.getOrderNumber(list);
+  }
+
 
   return (
     <section className={styleConstructor.container}>
@@ -25,21 +38,25 @@ function BurgerConstructor({ data, itemDom }) {
             isLocked={true}
             text="Краторная булка N-200i (верх)"
             price={20}
-            thumbnail={data[0].image}
+            thumbnail={list[0].image}
           />
 
           <ul className={styleConstructor.list}>
             {
-              saucesAndMains.map((item) => (
-                <li className={styleConstructor.card} key={item._id}>
-                  <span className={styleConstructor.buttonDrag}><DragIcon type="primary" /></span>
-                  <ConstructorElement
-                    text={item.name}
-                    price={item.price}
-                    thumbnail={item.image}
-                  />
-                </li>
-              ))}
+              saucesAndMains.map((item) => {
+
+                return (
+                  <li className={styleConstructor.card} key={item._id}>
+                    <span className={styleConstructor.buttonDrag}><DragIcon type="primary" /></span>
+                    <ConstructorElement
+                      text={item.name}
+                      price={item.price}
+                      thumbnail={item.image}
+                    />
+                  </li>
+                )
+              })
+            }
           </ul>
 
           <ConstructorElement
@@ -47,17 +64,20 @@ function BurgerConstructor({ data, itemDom }) {
             isLocked={true}
             text="Краторная булка N-200i (низ)"
             price={20}
-            thumbnail={data[0].image}
+            thumbnail={list[0].image}
           />
         </div>
 
 
         <div className={styleConstructor.order + " mt-10 mr-4 pb-15"}>
           <span className={"text text_type_digits-medium mr-10"}>
-            {'1200'}
+            {totalPrice}
             <span className="ml-2"><CurrencyIcon type="primary" /></span>
           </span>
-          <Button htmlType="button" type="primary" size="large" onClick={handleOrderClick}>
+          <Button htmlType="button" type="primary" size="large" onClick={() => {
+            handleOrderNumber();
+            handleOrderClick();
+          }}>
             Оформить заказ
           </Button>
         </div>
@@ -75,7 +95,6 @@ function BurgerConstructor({ data, itemDom }) {
 }
 
 BurgerConstructor.propTypes = {
-  data: PropTypes.array,
   itemDom: PropTypes.object,
 }
 
--- src/components/order-details/order-details.js
+++ src/components/order-details/order-details.js
@@ -1,12 +1,16 @@
 import styleOrder from "./order-details.module.css"
 import iconOrder from "../../images/done.svg";
+import { useContext } from "react";
+import { BurgerConstructorContext } from "../../services/context/burger-constructor-context";
 
 
 function OrderDetails() {
+  const constructorContext = useContext(BurgerConstructorContext);
+  const numberOrder = constructorContext?.statusOrder?.order?.number;
 
   return (
     <div className={styleOrder.container + " mt-15"}>
-      <h2 className="text text_type_digits-large mt-8 mb-8">034536</h2>
+      <h2 className="text text_type_digits-large mt-8 mb-8">{numberOrder}</h2>
       <p className="text text_type_main-medium mb-15 pb-1">идентификатор заказа</p>
       <img src={iconOrder} alt="иконка подтверждения" />
       <p className="text text_type_main-default mt-15 pt-3 mb-2">Ваш заказ начали готовить</p>
