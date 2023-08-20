f60d695113fcda8c2d9c7b3c98fa94ceb6b0b5c0
vitali <vitali.slepnyov@gmail.com>
2023-08-20 03:14:24 +0300

    refactor: изменить модальные окна, удалить контекст, изменить IngredientDetails

--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -1,39 +1,40 @@
-import PropTypes from 'prop-types';
-import styleDetails from "./ingredient-details.module.css"
+import styleDetails from "./ingredient-details.module.css";
+import { useSelector } from "react-redux";
 
 
-function IngredientDetails({ ingredientInfo }) {
+function IngredientDetails() {
+
+  const { selectedIngredient } = useSelector(state => state.ingredientDetails);
+
+  const { name, calories, carbohydrates, fat, proteins, image_large } = selectedIngredient;
 
   return (
     <section className={styleDetails.container}>
       <h2 className={styleDetails.title + " text text_type_main-large mt-6 ml-10"}>Детали ингредиента</h2>
-      <img src={ingredientInfo.image_large} alt={ingredientInfo.name} />
-      <p className="text text_type_main-medium mt-8 mb-8">{ingredientInfo.name}</p>
+      <img src={image_large} alt={name} />
+      <p className="text text_type_main-medium mt-8 mb-8">{name}</p>
 
       <ul className={styleDetails.list + " mb-10"}>
         <li className="text text_type_main-default text_color_inactive">
           <span className={styleDetails.block}>Калории,ккал</span>
-          <p className={"text text_type_digits-default pt-2"}>{ingredientInfo.calories}</p>
+          <p className={"text text_type_digits-default pt-2"}>{calories}</p>
         </li>
         <li className="text text_type_main-default text_color_inactive">
           <span className={styleDetails.block}>Белки, г</span>
-          <p className="text text_type_digits-default pt-2">{ingredientInfo.proteins}</p>
+          <p className="text text_type_digits-default pt-2">{proteins}</p>
         </li>
         <li className="text text_type_main-default text_color_inactive">
           <span className={styleDetails.block}>Жиры, г</span>
-          <p className="text text_type_digits-default pt-2">{ingredientInfo.fat}</p>
+          <p className="text text_type_digits-default pt-2">{fat}</p>
         </li>
         <li className="text text_type_main-default text_color_inactive">
           <span className={styleDetails.block}>Углеводы, г</span>
-          <p className="text text_type_digits-default pt-2">{ingredientInfo.carbohydrates}</p>
+          <p className="text text_type_digits-default pt-2">{carbohydrates}</p>
         </li>
       </ul>
     </section>
-  );
+  )
 }
 
-IngredientDetails.propTypes = {
-  ingredientInfo: PropTypes.object,
-};
 
 export default IngredientDetails;
--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -2,16 +2,18 @@ import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import PropTypes from 'prop-types';
-import ModalOverlay from '../modal-overlay/modal-overlay';
+import ModalOverlay from "../modal-overlay/modal-overlay";
 import styleModal from "./modal.module.css";
 
 
-function Modal(props) {
+function Modal({ children, closePopup }) {
+
+  const modalRoot = document.getElementById('root');
 
   useEffect(() => {
     const handleKeyDown = function (e) {
       if (e.key === 'Escape') {
-        props.closePopup();
+        closePopup();
       }
     };
     document.addEventListener('keydown', handleKeyDown);
@@ -23,24 +25,24 @@ function Modal(props) {
 
   return createPortal(
     <>
-      <ModalOverlay overlayCloseClick={props.closePopup} />
+      <ModalOverlay overlayCloseClick={closePopup} />
       <section className={styleModal.container}>
         <div className={styleModal.popup}>
-          <button className={styleModal.buttonClose} onClick={props.closePopup}>
+          <button className={styleModal.buttonClose} type="button" onClick={closePopup}>
             <CloseIcon type="primary" />
           </button>
-          {props.children}
+          {children}
         </div>
       </section>
     </>,
 
-    props.pointModal
+    modalRoot
   );
 }
 
 Modal.propTypes = {
+  children: PropTypes.element,
   closePopup: PropTypes.func,
-  pointModal: PropTypes.object,
-}
+};
 
 export default Modal;
