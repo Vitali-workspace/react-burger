be7b053ea0f11fbf3c4d50d5fe19be5c164cb837
vitali <vitali.slepnyov@gmail.com>
2023-09-02 23:47:54 +0300

    feat: добавить экшен login

---
+++ src/services/actions/action-login.js
@@ -0,0 +1,30 @@
+import requestApi from "../../utils/request-api";
+import { setCookie } from "../../utils/cookie-api";
+
+export const LOGIN_SUBMIT_REQUEST = "LOGIN_SUBMIT_REQUEST";
+export const LOGIN_SUBMIT_ERROR = "LOGIN_SUBMIT_ERROR";
+export const LOGIN_SUBMIT_SUCCESS = "LOGIN_SUBMIT_SUCCESS";
+
+export const token = { access: "accessToken", refresh: "refreshToken" };
+
+
+export function login(info) {
+
+  return function (dispatch) {
+    dispatch({ type: LOGIN_SUBMIT_REQUEST });
+
+    requestApi.login(info)
+      .then((data) => {
+        dispatch({ type: LOGIN_SUBMIT_SUCCESS, user: data.user });
+
+        setCookie(token.access, data.accessToken);
+        localStorage.setItem(token.refresh, data.refreshToken);
+      })
+      .catch((error) => {
+        dispatch({ type: LOGIN_SUBMIT_ERROR });
+
+        console.log(error);
+      })
+  }
+}
+
