2cce967cbc474267e7124dea5a40c274a24de408
vitali <vitali.slepnyov@gmail.com>
2023-09-02 22:05:45 +0300

    refactor: изменить компонент Profile

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,12 +1,13 @@
-import { Button, Input } from "@ya.praktikum/react-developer-burger-ui-components";
-import { Link, useNavigate } from 'react-router-dom';
+import { Input } from "@ya.praktikum/react-developer-burger-ui-components";
+import { NavLink } from 'react-router-dom';
 import styleProfile from "./profile.module.css";
 
 
 function Profile() {
 
-  function out() {
-
+  function exit() {
+    //dispatch();
+    console.log('ВЫход')
   }
 
   function submitForm(evt) {
@@ -14,20 +15,41 @@ function Profile() {
   }
 
   return (
-    <section className={styleProfile.container}>
+    <section className={`${styleProfile.container} mt-30`}>
       <div className={styleProfile.navigation}>
         <ul className={styleProfile.list}>
-          <li><Link className={styleProfile.link} to="/profile/orders">Профиль</Link></li>
-          <li><Link className={styleProfile.link} to="/profile/orders/:id">История заказов</Link></li>
-          <li><button className={styleProfile.link}>Выход</button></li>
+          <li>
+            <NavLink
+              className={`${styleProfile.link} text text_type_main-medium text_color_inactive`}
+              activeClassName={styleProfile.active}
+              to="/profile/orders"
+              exact>
+              Профиль
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={`${styleProfile.link} text text_type_main-medium text_color_inactive`}
+              activeClassName={styleProfile.active}
+              to="/profile/orders/:id">
+              История заказов
+            </NavLink>
+          </li>
+          <li>
+            <button
+              className={`${styleProfile.button} text text_type_main-medium text_color_inactive`}
+              onClick={exit}>
+              Выход
+            </button>
+          </li>
         </ul>
-        <p className="text text_type_main-default text_color_inactive">
+        <p className={`${styleProfile.description} text text_type_main-default text_color_inactive mt-20`}>
           В этом разделе вы можете изменить свои персональные данные
         </p>
       </div>
 
       <div>
-        <form className={styleProfile.inputs} onSubmit={"submitForm"}>
+        <form className={styleProfile.inputs} onSubmit={submitForm}>
           <Input
             type="text"
             name="name"
@@ -51,11 +73,6 @@ function Profile() {
             minLength={3}
             required
           />
-          <div className={styleProfile.button}>
-            <Button htmlType="button" type="primary" size="medium">
-              Зарегистрироваться
-            </Button>
-          </div>
 
         </form>
       </div>
