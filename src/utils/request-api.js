c4a9720f2d05b98aee106536be100f2451af4c1d
vitali <vitali.slepnyov@gmail.com>
2023-09-02 22:04:02 +0300

    feat: добавить основные api запросы

--- src/utils/request-api.js
+++ src/utils/request-api.js
@@ -1,11 +1,75 @@
-import { API_URL, API_URL_ORDER } from "./constants";
+import { API_URL, API_AUTH } from "./constants";
+import { token } from "../services/actions/action-login";
+import { getCookie } from "./cookie-api";
+
 
 class requestApi {
 
+  registration({ name, email, password }) {
+    return fetch(`${API_AUTH}/register`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, email, password }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        return Promise.reject(`произошла ошибка: ${response.status}`);
+      })
+      .catch(console.error);
+  }
+
+  login({ email, password }) {
+    return fetch(`${API_AUTH}/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        return Promise.reject(`произошла ошибка: ${response.status}`);
+      })
+      .catch(console.error);
+  }
+
+  logout() {
+    return fetch(`${API_AUTH}/logout`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ token: localStorage.getItem(token.refresh) }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        return Promise.reject(`произошла ошибка: ${response.status}`);
+      })
+      .catch(console.error);
+  }
+
+  updateToken() {
+    return fetch(`${API_AUTH}/token`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ token: localStorage.getItem(token.refresh) }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        return Promise.reject(`произошла ошибка: ${response.status}`);
+      })
+      .catch(console.error);
+  }
+
+
   getOrderNumber(listId) {
-    return fetch(API_URL_ORDER, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+    return fetch(`${API_URL}/orders`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ "ingredients": listId }),
     })
       .then((response) => {
@@ -18,8 +82,8 @@ class requestApi {
   }
 
   getIngredients() {
-    return fetch(API_URL, {
-      method: 'GET'
+    return fetch(`${API_URL}/ingredients`, {
+      method: "GET"
     })
       .then((response) => {
         if (response.ok) {
@@ -30,8 +94,78 @@ class requestApi {
       .catch(console.error);
   }
 
+
+  getUserInfo() {
+    return fetch(`${API_AUTH}/profile`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": getCookie(token.access),
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        return Promise.reject(`произошла ошибка: ${response.status}`);
+      })
+      .catch(console.error);
+  }
+
+
+  updateUserInfo({ name, email, password }) {
+    return fetch(`${API_AUTH}/profile`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": getCookie(token.access),
+      },
+      body: JSON.stringify({ name, email, password }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        return Promise.reject(`произошла ошибка: ${response.status}`);
+      })
+      .catch(console.error);
+  }
+
+
+  resetPassword({ code, password }) {
+    return fetch(`${API_URL}/password-reset/reset`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ code, password }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        return Promise.reject(`произошла ошибка: ${response.status}`);
+      })
+      .catch(console.error);
+  }
+
+
+  forgotPassword({ email }) {
+    return fetch(`${API_URL}/password-reset`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        return Promise.reject(`произошла ошибка: ${response.status}`);
+      })
+      .catch(console.error);
+  }
+
+
 }
 
-const api = new requestApi(API_URL, API_URL_ORDER);
+const api = new requestApi(API_URL);
 
 export default api;
