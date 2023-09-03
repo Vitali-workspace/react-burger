0e100a198f606217b45adf83612580cd34a5d04b
vitali <vitali.slepnyov@gmail.com>
2023-09-03 19:02:11 +0300

    refactor: изменить работу Profile

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,18 +1,24 @@
-import { Input } from "@ya.praktikum/react-developer-burger-ui-components";
-import { NavLink } from 'react-router-dom';
+
+import { NavLink, useNavigate } from 'react-router-dom';
+import { useDispatch } from "react-redux";
+import { logout } from "../../services/actions/action-profile";
+import ProfileUser from '../profile-user/profile-user';
 import styleProfile from "./profile.module.css";
 
 
 function Profile() {
 
+  const styleLink = `${styleProfile.link} text text_type_main-medium text_color_inactive`;
+  const styleLinkActive = `${styleProfile.active} text text_type_main-medium`;
+  const dispatch = useDispatch();
+  const navigation = useNavigate();
+
   function exit() {
-    //dispatch();
+    dispatch(logout());
+    navigation("/");
     console.log('ВЫход')
   }
 
-  function submitForm(evt) {
-    evt.preventDefault();
-  }
 
   return (
     <section className={`${styleProfile.container} mt-30`}>
@@ -20,17 +26,14 @@ function Profile() {
         <ul className={styleProfile.list}>
           <li>
             <NavLink
-              className={`${styleProfile.link} text text_type_main-medium text_color_inactive`}
-              activeClassName={styleProfile.active}
-              to="/profile/orders"
-              exact>
+              className={({ isActive }) => (isActive ? styleLinkActive : styleLink)}
+              to="/profile">
               Профиль
             </NavLink>
           </li>
           <li>
             <NavLink
-              className={`${styleProfile.link} text text_type_main-medium text_color_inactive`}
-              activeClassName={styleProfile.active}
+              className={({ isActive }) => (isActive ? styleLinkActive : styleLink)}
               to="/profile/orders/:id">
               История заказов
             </NavLink>
@@ -43,39 +46,11 @@ function Profile() {
             </button>
           </li>
         </ul>
-        <p className={`${styleProfile.description} text text_type_main-default text_color_inactive mt-20`}>
+        <p className={`${styleProfile.description} text text_type_main-default mt-20`}>
           В этом разделе вы можете изменить свои персональные данные
         </p>
       </div>
-
-      <div>
-        <form className={styleProfile.inputs} onSubmit={submitForm}>
-          <Input
-            type="text"
-            name="name"
-            placeholder="Имя"
-            required
-          />
-          <Input
-            type="email"
-            name="email"
-            placeholder="E-mail"
-            // value={"email" || ""}
-            // onChange={"handleChangeInput"}
-            required
-          />
-          <Input
-            type="password"
-            name="password"
-            placeholder="Пароль"
-            // value={"password" || ''}
-            // onChange={"handleChangeInput"}
-            minLength={3}
-            required
-          />
-
-        </form>
-      </div>
+      <ProfileUser />
     </section>
   )
 }
