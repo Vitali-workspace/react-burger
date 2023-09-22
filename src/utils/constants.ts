d0dbb8240459f2d335ba89a777768c720bda0ce3
vitali <vitali.slepnyov@gmail.com>
2023-09-22 23:50:17 +0300

    refactor: переписать код на typescript

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,22 +1,5 @@
-import PropTypes from "prop-types";
 
 export const BASE_URL = "https://norma.nomoreparties.space/api";
-
-
-export const ingredientPropTypes = PropTypes.shape({
-  _id: PropTypes.string,
-  name: PropTypes.string,
-  type: PropTypes.string,
-  proteins: PropTypes.number,
-  fat: PropTypes.number,
-  carbohydrates: PropTypes.number,
-  calories: PropTypes.number,
-  price: PropTypes.number,
-  image: PropTypes.string,
-  image_large: PropTypes.string,
-  uuid: PropTypes.string,
-});
-
 
 export const TYPE_BUN = {
   TOP: "top",
@@ -41,7 +24,4 @@ export const TYPE_INGREDIENT = {
   SAUCE: "sauce",
 };
 
-export const CONFIG_SCROLL = {
-  behavior: "smooth",
-  block: "start",
-};
+
