0b64b105eb804979215bdd240ec038817456dd7e
vitali <vitali.slepnyov@gmail.com>
2023-09-22 23:19:56 +0300

    refactor: переписать код на typescript

--- src/utils/cookie-api.ts
+++ src/utils/cookie-api.ts
@@ -1,4 +1,4 @@
-export function setCookie(name, value, props) {
+export function setCookie(name: string, value: string, props: any) {
 
   props = props || {};
   let exp = props.expires;
@@ -27,12 +27,12 @@ export function setCookie(name, value, props) {
 }
 
 
-export function deleteCookie(name) {
-  setCookie(name, null, { expires: -1 });
+export function deleteCookie(name: string) {
+  setCookie(name, '', { expires: -1 });
 }
 
 
-export function getCookie(name) {
+export function getCookie(name: string) {
 
   const matches = document.cookie.match(
     new RegExp('(?:^|; )' + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + '=([^;]*)')
