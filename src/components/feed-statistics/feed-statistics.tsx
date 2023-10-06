94a9321d7b0fe6cf4585f0ec4c104211864afc63
vitali <vitali.slepnyov@gmail.com>
2023-10-06 23:23:21 +0300

    feat: добавить компонент FeedStatistics

--- src/components/feed-statistics/feed-statistics.tsx
+++ src/components/feed-statistics/feed-statistics.tsx
@@ -5,31 +5,37 @@ import styleStatistics from "./feed-statistics.module.css"
 const FeedStatistics: FC = () => {
 
   return (
-    <section className={styleStatistics.container + " ml-15 mt-10"}>
+    <section className={styleStatistics.container + " ml-15 mt-15"}>
 
       <div className={styleStatistics.header}>
-        <div>
-          <h3 className="text text_type_main-medium">Готовы:</h3>
-          <ul>
-            <li></li>
+        <div className={styleStatistics.block}>
+          <h3 className="text text_type_main-medium mb-6">Готовы:</h3>
+          <ul className={styleStatistics.list + " text text_type_digits-default " + styleStatistics.success}>
+            <li>034535</li>
+            <li>034535</li>
+            <li>034535</li>
+            <li>034535</li>
+            <li>034535</li>
           </ul>
         </div>
 
-        <div>
-          <h3 className="text text_type_main-medium">В работе:</h3>
-          <ul>
-            <li></li>
+        <div className={styleStatistics.block}>
+          <h3 className="text text_type_main-medium mb-6">В работе:</h3>
+          <ul className={"text text_type_digits-default " + styleStatistics.list}>
+            <li>034535</li>
+            <li>034535</li>
+            <li>034535</li>
           </ul>
         </div>
       </div>
 
       <div>
         <h3 className="text text_type_main-medium mt-15">Выполнено за всё время:</h3>
-        <span className="text text_type_digits-large">3</span>
+        <span className="text text_type_digits-large">28753</span>
       </div>
       <div>
         <h3 className="text text_type_main-medium mt-15">Выполнено за сегодня:</h3>
-        <span className="text text_type_digits-large">1</span>
+        <span className="text text_type_digits-large">138</span>
       </div>
 
     </section>
