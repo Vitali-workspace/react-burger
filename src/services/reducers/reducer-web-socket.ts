594d888cffd76d4e8200a656dcf3faf6be2bda33
vitali <vitali.slepnyov@gmail.com>
2023-10-07 23:44:42 +0300

    feat: добавить редьюсер

---
+++ src/services/reducers/reducer-web-socket.ts
@@ -0,0 +1,72 @@
+import { TOrders } from "../types/services-types";
+import { TWSActions } from "../actions/action-web-socket";
+export const WS_CONNECTION_START: 'WS_CONNECTION_START' = 'WS_CONNECTION_START';
+export const WS_CONNECTION_SUCCESS: 'WS_CONNECTION_SUCCESS' = 'WS_CONNECTION_SUCCESS';
+export const WS_CONNECTION_ERROR: 'WS_CONNECTION_ERROR' = 'WS_CONNECTION_ERROR';
+export const WS_CONNECTION_CLOSED: 'WS_CONNECTION_CLOSED' = 'WS_CONNECTION_CLOSED';
+export const WS_GET_ORDERS: 'WS_GET_ORDERS' = 'WS_GET_ORDERS';
+
+
+interface IWSState {
+  wsConnected: boolean;
+  orders: TOrders;
+  userOrders: TOrders;
+  total: number;
+  totalToday: number;
+  loading: boolean;
+  error?: Event;
+}
+
+const initialState: IWSState = {
+  wsConnected: false,
+  orders: [],
+  userOrders: [],
+  total: 0,
+  totalToday: 0,
+  loading: false,
+};
+
+
+export const reducerWebSocket = (state = initialState, action: TWSActions) => {
+
+  switch (action.type) {
+
+    case WS_GET_ORDERS: {
+      return {
+        ...state,
+        error: undefined,
+        orders: action.payload.orders ? action.payload.orders : state.orders,
+        total: action.payload.total,
+        totalToday: action.payload.totalToday,
+        loading: false,
+      };
+    }
+    case WS_CONNECTION_START: {
+      return { ...state, loading: true }
+    }
+    case WS_CONNECTION_SUCCESS: {
+      return {
+        ...state,
+        error: undefined,
+        wsConnected: true,
+      };
+    }
+    case WS_CONNECTION_CLOSED: {
+      return {
+        ...state,
+        error: undefined,
+        wsConnected: false,
+        loading: false,
+      };
+    }
+    case WS_CONNECTION_ERROR: {
+      return {
+        ...state,
+        error: action.payload,
+        wsConnected: false,
+        loading: false,
+      };
+    }
+    default: { return state }
+  }
+}
