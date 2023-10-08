a4c76d154eb8d236608b2bc9fe9d3e881d55bcc1
vitali <vitali.slepnyov@gmail.com>
2023-10-08 23:49:06 +0300

    refactor: типизировать экшены login

--- src/services/actions/action-login.ts
+++ src/services/actions/action-login.ts
@@ -1,5 +1,7 @@
 import RequestApi from "../../utils/request-api";
 import { setCookie } from "../../utils/cookie-api";
+import { IForm } from "../types/services-types";
+import { AppDispatch, AppThunkAction } from "../types/services-types";
 
 export const LOGIN_SUBMIT_REQUEST = "LOGIN_SUBMIT_REQUEST";
 export const LOGIN_SUBMIT_ERROR = "LOGIN_SUBMIT_ERROR";
@@ -7,24 +9,50 @@ export const LOGIN_SUBMIT_SUCCESS = "LOGIN_SUBMIT_SUCCESS";
 
 export const token = { access: "accessToken", refresh: "refreshToken" };
 
+export interface IActionLoginSubmitRequest {
+  readonly type: typeof LOGIN_SUBMIT_REQUEST;
+}
 
-export function login(info: any) {
+export interface IActionLoginSubmitError {
+  readonly type: typeof LOGIN_SUBMIT_ERROR;
+}
 
-  return function (dispatch: any) {
-    dispatch({ type: LOGIN_SUBMIT_REQUEST });
+export interface IActionLoginSubmitSuccess {
+  readonly type: typeof LOGIN_SUBMIT_SUCCESS;
+  readonly user: IForm;
+}
 
-    RequestApi.login(info)
-      .then((data) => {
-        dispatch({ type: LOGIN_SUBMIT_SUCCESS, user: data.user });
 
-        setCookie(token.access, data.accessToken, info);
-        localStorage.setItem(token.refresh, data.refreshToken);
-      })
-      .catch((error) => {
-        dispatch({ type: LOGIN_SUBMIT_ERROR });
+export type TActionsLogin =
+  | IActionLoginSubmitRequest
+  | IActionLoginSubmitError
+  | IActionLoginSubmitSuccess;
 
-        console.log(error);
-      })
-  }
+
+export const actionLoginSubmitRequest = (): IActionLoginSubmitRequest => ({ type: LOGIN_SUBMIT_REQUEST });
+
+export const actionLoginSubmitError = (): IActionLoginSubmitError => ({ type: LOGIN_SUBMIT_ERROR });
+
+export const actionLoginSubmitSuccess = (user: IForm): IActionLoginSubmitSuccess => ({
+  type: LOGIN_SUBMIT_SUCCESS,
+  user,
+});
+
+
+
+export const login: AppThunkAction = (info: any) => (dispatch: AppDispatch) => {
+  dispatch(actionLoginSubmitRequest());
+
+  RequestApi.login(info)
+    .then((data) => {
+      dispatch(actionLoginSubmitSuccess(data.user));
+      setCookie(token.access, data.accessToken, info);
+      localStorage.setItem(token.refresh, data.refreshToken);
+    })
+    .catch((error) => {
+      dispatch(actionLoginSubmitError());
+
+      console.log(error);
+    })
 }
 
