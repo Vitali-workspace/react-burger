79c8c069f6b323cc7533475e01e0f76fef32aef9
vitali <vitali.slepnyov@gmail.com>
2023-10-10 23:28:17 +0300

    refactor: изменить store

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,13 +1,13 @@
 import { applyMiddleware, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 import { rootReducer } from "./reducers";
-
+import { wsActions, middlewareWebSocket } from "./middleware/middleware-web-socket"; //! wsActions переименовать
 
 declare global {
   interface Window { __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose }
 }
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const enhancer = composeEnhancers(applyMiddleware(thunk)); //!
+const enhancer = composeEnhancers(applyMiddleware(thunk, middlewareWebSocket(wsActions)));
 
 export const store = createStore(rootReducer, enhancer);
