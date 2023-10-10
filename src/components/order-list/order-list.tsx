308a5dba750ccb9ce10e849c4757584cbba2ad1f
vitali <vitali.slepnyov@gmail.com>
2023-10-10 23:52:03 +0300

    feat: добавить перебор массива в OrderList

--- src/components/order-list/order-list.tsx
+++ src/components/order-list/order-list.tsx
@@ -1,16 +1,22 @@
 import { FC } from "react";
-import OrderElement from "../order-element/order-element"; //! Для теста
+import { IOrderFeed } from "../../services/types/services-types";
+import OrderElement from "../order-element/order-element";
 import styleList from "./order-list.module.css"
 
+interface IOrderList { orders: IOrderFeed[] }
 
-const OrderList: FC = () => {
+
+const OrderList: FC<IOrderList> = ({ orders }) => {
+
+  const lastTenOrders = orders.slice(-10);
 
   return (
     <ul className={`${styleList.list} custom-scroll`} >
-      <OrderElement />
-      <OrderElement />
-      <OrderElement />
-      <OrderElement />
+      {
+        lastTenOrders.map((order) => (
+          <OrderElement order={order} key={order._id} />
+        ))
+      }
     </ul>
   )
 
