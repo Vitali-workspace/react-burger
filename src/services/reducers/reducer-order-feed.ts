0b377abdabb8cd95c4b5b9c3d67464586e4d47f9
vitali <vitali.slepnyov@gmail.com>
2023-10-10 23:08:19 +0300

    feat: добавить reducerOrderFeed

---
+++ src/services/reducers/reducer-order-feed.ts
@@ -0,0 +1,47 @@
+import { TActionsFeed } from "../actions/action-order-feed";
+import { IOrderFeed } from "../types/services-types";
+
+export const REJECT_FEED_ORDER = "REJECT_FEED_ORDER";
+export const SELECT_FEED_ORDER = "SELECT_FEED_ORDER";
+export const STATUS_ORDERS = "STATUS_ORDERS";
+export const CHECK_ORDERS = "CHECK_ORDERS";
+
+
+interface IStateOrderFeed {
+  orders: IOrderFeed[],
+  selectedOrder: IOrderFeed | null;
+  ready: number[],
+  pending: number[],
+}
+
+const initialState: IStateOrderFeed = {
+  orders: [],
+  selectedOrder: null,
+  ready: [],
+  pending: [],
+};
+
+
+export const reducerOrderFeed = (state = initialState, action: TActionsFeed): IStateOrderFeed => {
+
+  switch (action.type) {
+
+    case REJECT_FEED_ORDER: {
+      return { ...state, selectedOrder: null }
+    }
+    case SELECT_FEED_ORDER: {
+      return { ...state, selectedOrder: action.payload };
+    }
+    case STATUS_ORDERS: {
+      return {
+        ...state,
+        ready: action.payload.ready,
+        pending: action.payload.pending,
+      }
+    }
+    case CHECK_ORDERS: {
+      return { ...state, orders: action.payload };
+    }
+    default: { return state }
+  }
+}
