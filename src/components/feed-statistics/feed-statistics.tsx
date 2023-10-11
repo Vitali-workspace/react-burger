3f61ea16aceb16c8d2cce1b2ca4038aac56c45c4
vitali <vitali.slepnyov@gmail.com>
2023-10-11 23:24:13 +0300

    feat: добавить функцию подсчёта номеров

--- src/components/feed-statistics/feed-statistics.tsx
+++ src/components/feed-statistics/feed-statistics.tsx
@@ -1,8 +1,15 @@
 import { FC } from "react";
+import { useAppSelector } from "../../services/hooks/services-hooks";
 import styleStatistics from "./feed-statistics.module.css"
 
 
 const FeedStatistics: FC = () => {
+
+  const { ready, pending } = useAppSelector((state) => state.oderFeed);
+  const { total, totalToday } = useAppSelector((state) => state.webSocket);
+
+  const maxOrdersReady = ready.slice(0, 20);
+  const maxOrdersPending = pending.slice(0, 20);
 
   return (
     <section className={styleStatistics.container + " ml-15 mt-15"}>
@@ -11,31 +18,29 @@ const FeedStatistics: FC = () => {
         <div className={styleStatistics.block}>
           <h3 className="text text_type_main-medium mb-6">Готовы:</h3>
           <ul className={styleStatistics.list + " text text_type_digits-default " + styleStatistics.success}>
-            <li>034535</li>
-            <li>034535</li>
-            <li>034535</li>
-            <li>034535</li>
-            <li>034535</li>
+            {
+              maxOrdersReady.map((number, index) => (<li key={index}>{number}</li>))
+            }
           </ul>
         </div>
 
         <div className={styleStatistics.block}>
           <h3 className="text text_type_main-medium mb-6">В работе:</h3>
           <ul className={"text text_type_digits-default " + styleStatistics.list}>
-            <li>034535</li>
-            <li>034535</li>
-            <li>034535</li>
+            {
+              maxOrdersPending.map((number, index) => (<li key={index}>{number}</li>))
+            }
           </ul>
         </div>
       </div>
 
       <div>
         <h3 className="text text_type_main-medium mt-15">Выполнено за всё время:</h3>
-        <span className="text text_type_digits-large">28753</span>
+        <span className="text text_type_digits-large">{total}</span>
       </div>
       <div>
         <h3 className="text text_type_main-medium mt-15">Выполнено за сегодня:</h3>
-        <span className="text text_type_digits-large">138</span>
+        <span className="text text_type_digits-large">{totalToday}</span>
       </div>
 
     </section>
